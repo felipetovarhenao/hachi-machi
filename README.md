65439573aa5f75c843e22d95a1e7196d3dff56a8
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2026-04-16 15:08:25 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# LORO
+
+## Python setup
+
+1. Install [conda](https://anaconda.org/):
+2. In the terminal, run:
+
+```sh
+conda create -n loro python=3.10
+conda activate loro
+conda install pytorch tqdm
+conda install mido --channel conda-forge
+```
+
+## Before each session
+
+```sh
+conda activate loro
+```
---
+++ src/python/midi.py
@@ -0,0 +1,65 @@
+import os
+import mido
+import torch
+
+
+class MidiParser:
+    def __init__(self, file: str):
+        file_path = os.path.abspath(file)
+        ext = os.path.splitext(file_path)[1]
+        if ext not in ['.mid', '.midi']:
+            raise TypeError(
+                f"Invalid file extension: {ext}. Expected .mid or .midi")
+        midi = mido.MidiFile(filename=file)
+        if midi.type == 2:
+            raise TypeError(
+                'Invalid MIDI file type 2. Expected type 0 (single track) or 1 (multi-track).')
+        midi.tracks = [mido.merge_tracks(midi.tracks)]
+        self._midi = midi
+        self._events = self._parse()
+
+    def events(self):
+        return self._events
+
+    def _parse(self) -> torch.Tensor:
+        if self._events is not None:
+            return self._events
+        onset = 0
+        last_onset = 0
+        last_onset_per_voice = {}
+        events = []
+        voice_map = {}
+        voice_count = 0
+        active_notes = {}
+
+        for msg in self._midi:
+            onset += int(round(msg.time * 1000))
+            if not msg.type.startswith('note_'):
+                continue
+            channel = msg.channel
+            if channel not in voice_map:
+                voice_map[channel] = voice_count
+                voice_count += 1
+            pitch = int(round(msg.note * 100))
+            velocity = msg.velocity
+            voice = voice_map[channel]
+            key = (voice, pitch)
+            if velocity > 0:
+                ioi = onset - last_onset
+                last_onset = onset
+                voice_ioi = onset - last_onset_per_voice.get(voice, 0)
+                last_onset_per_voice[voice] = onset
+                active_notes[key] = [onset,
+                                     voice,
+                                     pitch,
+                                     velocity,
+                                     ioi,
+                                     voice_ioi]
+            elif key in active_notes:
+                event: list = active_notes.pop(key)
+                duration = onset - event[0]
+                event.append(duration)
+                events.append((event[0], event[1:]))
+
+        events.sort(key=lambda x: x[0])
+        return torch.tensor([e for _, e in events]).round()
