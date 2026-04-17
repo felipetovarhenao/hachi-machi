bd29704c5dadc8cac1e68b2d479d17014202240d
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2026-04-17 15:32:33 -0500

    edit

--- src/python/loro/utils.py
+++ src/python/loro/utils.py
@@ -4,7 +4,12 @@ import torch
 import json
 
 
-DEVICE = torch.accelerator.current_accelerator()
+if torch.cuda.is_available():
+    DEVICE = torch.device('cuda')
+elif torch.backends.mps.is_available():
+    DEVICE = torch.device('mps')
+else:
+    DEVICE = torch.device('cpu')
 
 COLORS = {
     'success': (85, 219, 158),
