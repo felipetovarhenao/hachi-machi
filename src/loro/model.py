8c30413efebd1de4754d9d938826b9a164c8e642
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2026-04-20 23:47:28 -0500

    edit

--- src/loro/model.py
+++ src/loro/model.py
@@ -120,7 +120,7 @@ class RecurrentMDN(nn.Module):
         pi, mu, sigma, hidden = self.forward(x, hidden)
         log = torch.log(pi.squeeze())
         k = torch.multinomial(
-            torch.exp((log / temp).clip(-88, 88)), num_samples=1).item()
+            torch.exp((log / temp)), num_samples=1).item()
         y = torch.normal(mean=mu[0, 0, :, k],
                          std=sigma[0, 0, :, k]).unsqueeze(0).unsqueeze(0)
         return y, hidden
