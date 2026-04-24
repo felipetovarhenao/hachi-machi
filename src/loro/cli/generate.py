1731418aaa486be719fb11b0f8598305fc04bcea
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2026-04-24 12:55:52 -0500

    minor

--- src/loro/cli/generate.py
+++ src/loro/cli/generate.py
@@ -10,7 +10,7 @@ from ..utils import (device_option,
 
 @click.command()
 @click.argument('model', type=click.Path(exists=True, file_okay=True, dir_okay=False, resolve_path=True))
-@click.argument('output', type=click.Path(file_okay=True, dir_okay=False))
+@click.option('output', default='out.txt', help='Path to output file, either in MIDI or TXT format.', type=click.Path(file_okay=True, dir_okay=False))
 @click.option('--tokens',  default=100, help='Number of tokens to generate.')
 @click.option('--seed', default=0, help='Random seed.')
 @click.option('--temp', default=1, help='Temperature.')
