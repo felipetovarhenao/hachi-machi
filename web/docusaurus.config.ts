fe43339be63207cecf036edf287614fe82da7198
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2026-05-30 16:26:53 -0400

    Add domain info

--- web/docusaurus.config.ts
+++ web/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://felipetovarhenao.github.io",
+  url: "https://hachi-machi.com/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/hachi-machi/",
